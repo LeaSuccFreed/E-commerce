0a4a614c36b71b684abc373b5d10d8b6b3abb903
LeaSuccFree <papanb.dev@gmail.com>
2020-05-14 19:32:04 +0200

    update our menu-items to navigate withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './App.scss';
-import HomePage from './Pages/homepage/homepage_component'
+import HomePage from './Pages/homepage/homepage_component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
--- src/homepage_component.jsx
+++ src/homepage_component.jsx
@@ -1,45 +1,12 @@
 import React from 'react';
 
+
 import './Pages/homepage/homepage_style.scss'
+import Directory from './Components/directory/directory_component';
 
 const HomePage = () => (
     <div className="hompage">
-        <div className='directory-menu'>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">HATS</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JACKETS</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">SNEAKERS</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">WOMENS</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">MENS</h1>
-                    <span className="subtitle">Shop Now</span>
-                </div>
-            </div>
-        </div>
+       <Directory></Directory>
     </div>
 )
 
