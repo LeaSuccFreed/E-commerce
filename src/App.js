5604bb99e9da226c1928804c13692647316578b6
LeaSuccFree <papanb.dev@gmail.com>
2020-05-15 19:05:39 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.scss';
 
 import HomePage from './Pages/homepage/homepage_component';
 import ShopPage from './Pages/shop/shop_component';
+import Header from './Components/header/header_component';
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop' component={ShopPage}></Route>
