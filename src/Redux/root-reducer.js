b5a39d68cfdde9b54a0387f0bdae21102801084b
LeaSuccFree <papanb.dev@gmail.com>
2020-05-30 19:30:19 +0200

    add directory state to redux

--- src/Redux/root-reducer.js
+++ src/Redux/root-reducer.js
@@ -1,8 +1,21 @@
  import {combineReducers} from  'redux'
 import userReducer from './User/user-reducer'
 import cartReducer from './Cart/cart_reducer'
+import directoryReducer from './Directory/directory-reducer';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
- export default combineReducers({
-     user: userReducer,
-     cart: cartReducer
- })
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    cart: cartReducer,
+    directory: directoryReducer
+})
+
+ export default persistReducer(persistConfig, rootReducer)
--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
+
 import App from './App';
-import store from './Redux/store'
+import {store, persistor} from './Redux/store'
 
 ReactDOM.render(
   
   <React.StrictMode>
     <Provider store={store}> 
       <BrowserRouter>
+        <PersistGate persistor={persistor}>
           <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
