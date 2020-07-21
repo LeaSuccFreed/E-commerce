68868a0a6c39fde0a0c7a0899858c55d7064036f
LeaSuccFreed <papanb.dev@gmail.com>
2020-07-20 22:36:21 -0700

    everything

--- client/src/Pages/homepage/homepage_component.jsx
+++ client/src/Pages/homepage/homepage_component.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+    import React , {Profiler} from 'react';
 
 import Directory from '../../Components/directory/directory_component'
 
 import {HomePageContainer} from './homepage_style'
 
 const HomePage = () => (
-        <HomePageContainer>
-        <Directory />
+        
+    <HomePageContainer>
+        <Profiler id="Directory" onRender={(id, phase, actualDuration) => 
+            console.log({
+                id,
+                phase,
+                actualDuration
+            })
+        }>
+            <Directory />
+        </Profiler>
     </HomePageContainer>
     ) 
 
